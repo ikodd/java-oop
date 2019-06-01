346c97550bbbb48d323048bf2a96687054d71874
ikodd <51268141+ikodd@users.noreply.github.com>
2019-06-01 18:28:45 +0300

    Add files via upload

---
+++ Triangle.java
@@ -0,0 +1,42 @@
+public class Triangle {
+private double sideA;
+private double sideB;
+private double sideC;
+
+    public Triangle(double sideA, double sideB, double sideC) {
+        this.sideA = sideA;
+        this.sideB = sideB;
+        this.sideC = sideC;
+    }
+    public double getSideA(){
+        return sideA;
+    }
+
+    public void setSideA(double sideA) {
+        this.sideA = sideA;
+    }
+
+    public void setSideB(double sideB) {
+        this.sideB = sideB;
+    }
+
+    public double getSideB() {
+        return sideB;
+    }
+
+    public double getSideC() {
+        return sideC;
+    }
+
+    public void setSideC(double sideC) {
+        this.sideC = sideC;
+    }
+
+   /*public boolean validTri(){
+           return sideA+sideB>sideC&&sideB+sideC>sideA&&sideC+sideA>sideB?true:false;
+   }*/
+void info(){
+    System.out.println("Стороны треугольника:");
+    System.out.println("a = "+sideA+"\r\nb = "+sideB+"\r\nc = "+sideC);
+}
+}
