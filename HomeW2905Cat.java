f191914131e3a4d12de17952e982ce0271a283bd
ikodd <51268141+ikodd@users.noreply.github.com>
2019-06-02 18:04:33 +0300

    Update HomeW2905Cat.java

--- HomeW2905Cat.java
+++ HomeW2905Cat.java
@@ -1,9 +1,10 @@
 public class HomeW2905 {
     public static void main(String[] args){
-        Cat cat1 = new Cat("Мурзик","кот", "британская", "короткая", 5,4.5,"пепельный",true);
-        Cat cat2 = new Cat("Мурка","кошка", "сиамская", "короткая", 3,5,"светло-коричневый",true);
+     Cat cat1 = new Cat("Мурзик","мужской", "британская", "короткая", 5,4.5,"пепельный",true);
+        Cat cat2 = new Cat("Мурка","женский", "сиамская", "короткая", 3,5,"светло-коричневый",true);
         cat1.print();
 cat1.state();
 cat2.print();
 cat2.state();
 cat2.meow();
+    }
