686b8ad8f6857cd504313943f5983ce9cbcc4303
ikodd <51268141+ikodd@users.noreply.github.com>
2019-06-05 17:21:56 +0300

    Static Main class for Triangle, Rectangle, Circle

---
+++ HomeW0306.java
@@ -0,0 +1,22 @@
+public class HomeW0306 {
+    public static void main(String[] args){
+        Point point1 = new Point(0,0);
+        Point point2 = new Point(0,3);
+        Point point3 = new Point (3,3);
+        Point point4 = new Point(3,0);
+
+
+        Triangle1 tri1 = new Triangle1(point1,point2,point3);
+        System.out.println("Периметр треугольника1: P = " + tri1.calcPerim());
+        System.out.println("Площадь треугольника1: S = "+tri1.calcArea());
+
+        Rectangle rec1 = new Rectangle(point1,point2,point3);
+        System.out.println("Периметр прямоугольника1: P = " + rec1.calcPerim());
+        System.out.println("Площадь прямоугольника1: S = "+rec1.calcArea());
+
+        Circle cir1 = new Circle(point1,point2);
+        System.out.println("Периметр окружности1: P = " + cir1.calcPerim());
+        System.out.println("Площадь окружности1: S = "+cir1.calcArea());
+
+    }
+}
