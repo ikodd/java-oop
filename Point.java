e7042add4fe2542e584f56fdb6e9b52ea33eab1c
ikodd <51268141+ikodd@users.noreply.github.com>
2019-06-05 17:17:17 +0300

    Point subclass of Shape superclass

---
+++ Point.java
@@ -0,0 +1,48 @@
+public class Point extends Shape{
+    private double x;
+    private double y;
+
+    public Point(double x, double y) {
+        super();
+        this.x = x;
+        this.y = y;
+    }
+
+    public Point(){
+        super();
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    @Override
+    public String toString() {
+        return "Point{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
+    }
+
+    @Override
+    public double calcPerim(double x, double y){
+        return 0;
+    }
+
+    @Override
+    public double calcArea(double x,double y){
+       return 0;
+    }
+}
