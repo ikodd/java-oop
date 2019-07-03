27209717a31c5802fdcebc7fada7bdc3698cffa3
ikodd <51268141+ikodd@users.noreply.github.com>
2019-07-03 17:34:51 +0300

    Add files via upload

---
+++ TenElementsArrayList/HomeW01071.java
@@ -0,0 +1,12 @@
+package joop.collections;
+
+public class HomeW01071 {
+    public static void main(String[] args){
+Integer[] arrI = {1,2,3,4,5,6,7,8,9,10,11,12};
+Character[] arrCh ={'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+        TenElementsAL <Integer> al1 = new TenElementsAL<>();
+        al1.tenElAl(arrI);
+        TenElementsAL <Character> al2 = new TenElementsAL<>();
+        al2.tenElAl(arrCh);
+    }
+}
---
+++ TenElementsArrayList/TenElementsAL.java
@@ -0,0 +1,39 @@
+package joop.collections;
+
+import java.util.ArrayList;
+
+public class TenElementsAL<T> {
+    private ArrayList<T> al = new ArrayList<>();
+
+    public TenElementsAL(ArrayList<T> al) {
+        super();
+        this.al = al;
+    }
+    public TenElementsAL() {
+        super();
+    }
+
+    public ArrayList<T> getAl() {
+        return al;
+    }
+
+    public void setAl(ArrayList<T> al) {
+        this.al = al;
+    }
+
+    @Override
+    public String toString() {
+        return "al=" + al;
+    }
+
+    public void tenElAl(T[] arr){
+if(arr.length < 10)return;
+for(int i = 0;i < 10;i++){
+getAl().add(arr[i]);
+}
+getAl().remove(0);
+getAl().remove(0);
+getAl().remove(getAl().size() - 1);
+        System.out.println(toString());;
+    }
+}
